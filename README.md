63085125b1d39c54ef1ad046f37455db2b12ed54
Maciej Bromirski <50320623+Nevalicjus@users.noreply.github.com>
2023-11-27 11:03:59 +0100

    added more langs

--- README.md
+++ README.md
@@ -2,12 +2,24 @@
 
 A small script/bin/app to run scripts 'my way' without using per-lang build files.
 
-Currently works for py and java.
+Currently works for c, cpp, elixir, java, go, js, kotlin, lua, python, ruby, rust, swift and v.
 
-#### py
+#### c
 ```bash
 #!/bin/bash
-python3 $1
+gcc -o out $1 && ./out && rm out
+```
+
+#### cpp
+```bash
+#!/bin/bash
+g++ $1 -o out && ./out && rm out
+```
+
+#### elixir
+```bash
+#!/bin/bash
+elixir $1
 ```
 
 #### java
@@ -15,4 +27,58 @@ python3 $1
 #!/bin/bash
 FNAME="$(awk '{split($0, array, "."); print array[1]}' <<< "$1")"
 javac $FNAME.java && java $FNAME && rm $FNAME.class
+```
+
+#### go
+```bash
+#!/bin/bash
+go run $1
+```
+
+#### js
+```bash
+#!/bin/bash
+node $1
+```
+
+#### kotlin
+```bash
+#!/bin/bash
+kotlinc $1 -include-runtime -d out.jar && java -jar out.jar && rm out.jar
+```
+
+#### lua
+```bash
+#!/bin/bash
+lua $1
+```
+
+#### py
+```bash
+#!/bin/bash
+python3 $1
+```
+
+#### ruby
+```bash
+#!/bin/bash
+ruby $1
+```
+
+#### rust
+```bash
+#!/bin/bash
+rustc $1 -o out && ./out && rm out
+```
+
+#### swift
+```bash
+#!/bin/bash
+swift $1
+```
+
+#### v
+```bash
+#!/bin/bash
+v run $1
 ```
--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,58 @@ fn java(name: &str, file: &str) {
 	let _ = Command::new("rm").arg(format!("{}.class", name)).status().expect("");
 }
 
+fn c(file: &str) {
+	let _ = Command::new("gcc").arg("-o").arg("out").arg(file).status().expect("");
+	let _ = Command::new("./out").status().expect("");
+	let _ = Command::new("rm").arg("out").status().expect("");
+}
+
+fn cpp(file: &str) {
+	let _ = Command::new("g++").arg("-o").arg("out").arg(file).status().expect("");
+	let _ = Command::new("./out").status().expect("");
+	let _ = Command::new("rm").arg("out").status().expect("");	
+}
+
+fn elixir(file: &str) {
+	let _ = Command::new("elixir").arg(file).status().expect("");
+}
+
+fn js(file: &str) {
+	let _ = Command::new("node").arg(file).status().expect("");
+}
+
+fn kotlin(file: &str) {
+	let _ = Command::new("kotlinc").arg(file).arg("-include-runtime").arg("-d").arg("out.jar").status().expect("");
+	let _ = Command::new("java").arg("-jar").arg("out.jar").status().expect("");
+	let _ = Command::new("rm").arg("out.jar").status().expect("");
+}
+
+fn lua(file: &str) {
+	let _ = Command::new("lua").arg(file).status().expect("");
+}
+
+fn ruby(file: &str) {
+	let _ = Command::new("ruby").arg(file).status().expect("");
+}
+
+fn rust(file: &str) {
+	let _ = Command::new("rustc").arg(file).arg("-o").arg("out").status().expect("");
+	let _ = Command::new("./out").status().expect("");
+	let _ = Command::new("rm").arg("out").status().expect("");
+}
+
+fn swift(file: &str) {
+	let _ = Command::new("swift").arg(file).status().expect("");
+}
+
+fn v(file: &str) {
+	let _ = Command::new("v").arg("run").arg(file).status().expect("");
+}
+
+fn go(file: &str) {
+	let _ = Command::new("go").arg("run").arg(file).status().expect("");
+}
+
 fn unsupported() {
 	println!("This file type is currently unsupported :(")
 }
@@ -32,8 +84,19 @@ fn main() {
     let ext = path.extension().and_then(OsStr::to_str).expect("");
     let file = &args[1];
     match ext {
-    	"py" => py(file),
+    	"c" => c(file),
+    	"cpp" => cpp(file),
+    	"exs" => elixir(file),
     	"java" => java(name, file),
+    	"go" => go(file),
+    	"js" | "ts" => js(file),
+    	"kt" => kotlin(file),
+    	"lua" => lua(file),
+    	"py" => py(file),
+    	"rb" => ruby(file),
+    	"rs" => rust(file),
+    	"swift" => swift(file),
+    	"v" => v(file),
     	&_ => unsupported()
     }
 }
