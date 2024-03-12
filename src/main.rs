b7ee9f80b4dfcdd8bb202cee80a1dfb813c04c3d
Maciej Bromirski <50320623+Nevalicjus@users.noreply.github.com>
2024-03-12 13:36:33 +0100

    added help, bumped to v0.5.0

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ fn main() {
     }
     let mut path = env::current_dir().expect("");
     path.push(&args[1]);
-    if !path.exists() {
+    if !path.exists() && args[1] != "-h" {
     	println!("File has to exist!");
         std::process::exit(1);
     }
@@ -28,26 +28,56 @@ fn main() {
     	config = HashMap::new();
     }
     let defaults: HashMap<String, String> = HashMap::from([
-    	("c".to_string(), "gcc -o out {file} && ./out && rm out".to_string()),
-    	("cpp".to_string(), "g++ -o out {file} && ./out && rm out".to_string()),
-    	("exs".to_string(), "elixir {file}".to_string()),
-    	("go".to_string(), "go run {file}".to_string()),
-    	("java".to_string(), "javac {file} && java {name} && rm {name}.class".to_string()),
-    	("js".to_string(), "node {file}".to_string()),
-    	("kt".to_string(), "kotlinc {file} -include-runtime -d out.jar && java -jar out.jar && rm out.jar".to_string()),
-		("lua".to_string(), "lua {file}".to_string()),
-		("py".to_string(), "python3 {file}".to_string()),
-		("rb".to_string(), "ruby {file}".to_string()),
-		("rs".to_string(), "rustc -o out {file} && ./out && rm out".to_string()),
-		("swift".to_string(), "swift {file}".to_string()),
-		("ts".to_string(), "node {file}".to_string()),
-		("v".to_string(), "v run {file}".to_string()),
-	]);
+        ("c".to_string(), "gcc -o out {file} && ./out && rm out".to_string()),
+        ("cpp".to_string(), "g++ -o out {file} && ./out && rm out".to_string()),
+        ("exs".to_string(), "elixir {file}".to_string()),
+        ("go".to_string(), "go run {file}".to_string()),
+        ("java".to_string(), "javac {file} && java {name} && rm {name}.class".to_string()),
+        ("js".to_string(), "node {file}".to_string()),
+        ("kt".to_string(), "kotlinc {file} -include-runtime -d out.jar && java -jar out.jar && rm out.jar".to_string()),
+    	("lua".to_string(), "lua {file}".to_string()),
+    	("py".to_string(), "python3 {file}".to_string()),
+    	("rb".to_string(), "ruby {file}".to_string()),
+    	("rs".to_string(), "rustc -o out {file} && ./out && rm out".to_string()),
+    	("swift".to_string(), "swift {file}".to_string()),
+    	("ts".to_string(), "node {file}".to_string()),
+    	("v".to_string(), "v run {file}".to_string()),
+    ]);
 	for (ext, cmd) in defaults.iter() {
 		if !config.contains_key(ext) {
 			config.insert(ext.clone(), cmd.clone());
 		}
 	}
+
+	if args.len() > 1 && args[1] == "-h" {
+	    println!("NevBuild (nb) v0.5.0 (c) Maciej Bromirski\n---");
+	    println!("nb takes the file you give it, gets it file extension, and if it has a");
+	    println!("command associated with it, it will run it (with substitutions: {{name}}, {{ext}}, {{file}})\n");
+	    println!("below you'll find the list of all configured associations,");
+	    println!("and if you have overwritten any, you'll also see what the default for them was");
+	    println!("you can overwrite or make new associations with a yaml file at ~/.nb.conf\n");
+	    let l: usize = config.keys().map(String::len).max().unwrap();
+	    let pad: String = " ".repeat(l);
+	    let mut keys: Vec<&String> = config.keys().collect();
+	    keys.sort();
+        for ext in keys {
+            let cmd: &String = config.get(ext).unwrap();
+            match defaults.get(ext) {
+                Some(d) => {
+                    if cmd != d {
+                        println!("{ext:<l$} -> {cmd}\n{pad} d: {d}");
+                    } else {
+                        println!("{ext:<l$} -> {cmd}");
+                    }
+                }
+                None => {
+                    println!("{ext:<l$} -> {cmd}");
+                }
+            }
+        }
+        std::process::exit(1);
+    }
+
     let name = path.file_stem().and_then(OsStr::to_str).expect("");
     let ext = path.extension().and_then(OsStr::to_str).expect("");
     let file = &args[1];
@@ -55,8 +85,9 @@ fn main() {
     .cloned()
     .unwrap_or_default()
     .replace("{file}", file)
-	.replace("{name}", name)
-	.replace("{ext}", ext);
+    .replace("{name}", name)
+    .replace("{ext}", ext);
+	    
 	if cmd.is_empty() {
 		println!("This file type is currently unsupported :(");
 		std::process::exit(1);
