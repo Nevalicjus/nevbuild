c4f92ca13ebb7dddf56ae6010103e4f3d31f74e8
Maciej Bromirski <maciej.bromirski@gmail.com>
2025-05-14 09:44:41 +0200

    minor fixes and bump to v0.7

--- README.md
+++ README.md
@@ -1,27 +1,46 @@
 # nevbuild
 
-A small script/bin/app to run scripts 'my way' without using per-lang build files.
+A small program to run scripts 'my way' without using per-lang build files.
 
-You can create a .nb.conf file in your $HOME directory to overwrite the defaults provided.
+You can create a .nb.conf file in your `$HOME` directory to overwrite the defaults provided.
 If you don't, assume they are this:
 
 ```yaml
 c: "gcc -o out {file} && ./out && rm out"
 cpp: "g++ -o out {file} && ./out && rm out"
-exs: "elixir {file}"
 go: "go run {file}"
 java: "javac {file} && java {name} && rm {name}.class"
 js: "node {file}"
 kt: "kotlinc {file} -include-runtime -d out.jar && java -jar out.jar && rm out.jar"
-lua: "lua {file}"
 py: "python3 {file}"
 rb: "ruby {file}"
 rs: "rustc -o out {file} && ./out && rm out"
 swift: "swift {file}"
 ts: "node {file}"
-v: "v run {file}"
 ```
 
 `{file}` -> full name, </br>
 `{name}` -> (without extension), </br>
 `{ext}` -> extension
+
+## Installation
+To install nb clone this repository and run `cargo build --release`.
+Afterwards, copy the built binary to your desired destination 
+(most likely `sudo cp target/release/nevbuild /usr/local/bin/nb`)
+
+## Usage
+To run a file using nb, simply use:
+```bash
+nb <file>
+```
+
+## Shebang
+nb can also be used as a [shebang](https://en.wikipedia.org/wiki/Shebang_(Unix)); simply prepend your file with:
+```bash
+#!/path/to/installed/nb
+```
+or
+```bash
+#!/usr/bin/env nb
+```
+and make it an executable (`chmod +x <file>`)
--- src/main.rs
+++ src/main.rs
@@ -2,105 +2,123 @@ use std::{
     collections::HashMap,
     env,
     ffi::OsStr,
-    fs::File,
+    fs,
     path::Path,
     process::{exit, Command}
 };
+use serde::{Serialize, Deserialize};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 2 {
-        println!("Usage: nb <file_path>");
-        exit(1);
-    }
-    let mut path = env::current_dir().expect("");
-    path.push(&args[1]);
-    if !path.exists() && args[1] != "-h" {
-    	println!("File has to exist!");
-        exit(1);
-    }
-    let home = env::var_os("HOME").expect("Cant get user's home");
-    let cffp = format!("{}/.nb.conf", home.to_str().unwrap());
-    let cfyaml = Path::new(&cffp);
-    let mut config: HashMap<String, String>;
-    if cfyaml.exists() {
-    	let cf = File::open(cffp).expect("Unable to open file");
-		config = serde_yaml::from_reader(cf).expect("Failed to parse YAML");
-    } else {
-    	config = HashMap::new();
-    }
-    let defaults: HashMap<String, String> = HashMap::from([
-        ("c".to_string(), "gcc -o out {file} && ./out && rm out".to_string()),
-        ("cpp".to_string(), "g++ -o out {file} && ./out && rm out".to_string()),
-        ("exs".to_string(), "elixir {file}".to_string()),
-        ("go".to_string(), "go run {file}".to_string()),
-        ("java".to_string(), "javac {file} && java {name} && rm {name}.class".to_string()),
-        ("js".to_string(), "node {file}".to_string()),
-        ("kt".to_string(), "kotlinc {file} -include-runtime -d out.jar && java -jar out.jar && rm out.jar".to_string()),
-    	("lua".to_string(), "lua {file}".to_string()),
-    	("py".to_string(), "python3 {file}".to_string()),
-    	("rb".to_string(), "ruby {file}".to_string()),
-    	("rs".to_string(), "rustc -o out {file} && ./out && rm out".to_string()),
-    	("swift".to_string(), "swift {file}".to_string()),
-    	("ts".to_string(), "node {file}".to_string()),
-    	("v".to_string(), "v run {file}".to_string()),
-    ]);
-	for (ext, cmd) in defaults.iter() {
-		if !config.contains_key(ext) {
-			config.insert(ext.clone(), cmd.clone());
-		}
-	}
-
-	if args.len() > 1 && args[1] == "-h" {
-	    println!("NevBuild (nb) v0.5.0 (c) Maciej Bromirski\n---");
-	    println!("nb takes the file you give it, gets it file extension, and if it has a");
-	    println!("command associated with it, it will run it (with substitutions: {{name}}, {{ext}}, {{file}})\n");
-	    println!("below you'll find the list of all configured associations,");
-	    println!("and if you have overwritten any, you'll also see what the default for them was");
-	    println!("you can overwrite or make new associations with a yaml file at ~/.nb.conf\n");
-	    let l: usize = config.keys().map(String::len).max().unwrap();
-	    let pad: String = " ".repeat(l);
-	    let mut keys: Vec<&String> = config.keys().collect();
-	    keys.sort();
-        for ext in keys {
-            let cmd: &String = config.get(ext).unwrap();
-            match defaults.get(ext) {
-                Some(d) => {
-                    if cmd != d {
-                        println!("{ext:<l$} -> {cmd}\n{pad} d: {d}");
-                    } else {
-                        println!("{ext:<l$} -> {cmd}");
+    let home = env::var_os("HOME").expect("/home/root/");
+    let conf: Config = Config::read_config(Path::new(&format!("{}/.nbconf", home.to_str().unwrap())));
+    match args.len() {
+        0 | 1 => {
+            println!("Usage: nb <file>");
+            exit(1);
+        }
+        2.. => {
+            if args[1] == "-h" {
+                print_help(&conf);
+            } else {
+                match eval_file(conf, &args) {
+                    Ok(_) => {
+                        exit(0);
                     }
-                }
-                None => {
-                    println!("{ext:<l$} -> {cmd}");
+                    Err(x) => {
+                        println!("Error: {}", x);
+                        exit(1);
+                    }
                 }
             }
         }
-        exit(1);
+    }
+}
+
+
+fn print_help(conf: &Config) {
+    let help_str: &'static str = "NevBuild (nb) v0.7.0 (c) Maciej Bromirski\n\
+        ---\n\
+        nb takes the file you give it, gets it file extension, and if it has a\n\
+        command associated with it, it will run it (with substitutions: {{name}}, {{ext}}, {{file}})\n\
+        \n\
+        below you'll find the list of all configured associations\n\
+        you can overwrite or make new associations with a yaml file at ~/.nb.conf\n\
+    ";
+    println!("{}", help_str);
+    for (ext, cmd) in conf.langs.iter() {
+        println!(" - .{} -> {}", ext, cmd);
+    }
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Config {
+    version: String,
+    langs: HashMap<String, String>
+}
+
+impl Config {
+    fn new() -> Config {
+        return Config {
+            version: String::from("0.0"),
+            langs: HashMap::new()
+        }
     }
 
+    fn default() -> Config {
+        return serde_yaml::from_str(include_str!("./../def_nb.conf")).unwrap();
+    }
+
+    fn from_file(path: &Path) -> Config {
+        if path.exists() == false {
+            return Config::new();
+        } else {
+            let f = fs::File::open(path).expect("Unable to open file");
+            return serde_yaml::from_reader(f).expect("Failed to parse yaml");
+        }
+    }
+
+    fn read_config(path: &Path) -> Config {
+        let mut conf: Config = Config::from_file(path);
+        let def_conf: Config = Config::default();
+        for (ext, cmd) in def_conf.langs.iter() {
+            if !conf.langs.contains_key(ext) {
+                conf.langs.insert(ext.clone(), cmd.clone());
+            }
+        }
+        return conf;
+    }
+}
+
+fn eval_file(conf: Config, args: &Vec<String>) -> Result<String, String> {
+    println!("nb: Evaluating file {}", args[1]);
+    let path = Path::new(&args[1]);
+    if !fs::exists(&path).unwrap() {
+        return Err(String::from("File doesn't exist"));
+    }
     let name = path.file_stem().and_then(OsStr::to_str).expect("");
     let ext = path.extension().and_then(OsStr::to_str).expect("");
     let file = &args[1];
-    let cmd: String = config.get(ext)
-    .cloned()
-    .unwrap_or_default()
-    .replace("{file}", file)
-    .replace("{name}", name)
-    .replace("{ext}", ext);
+    let cmd: String = conf.langs.get(ext)
+        .cloned()
+        .unwrap_or_default()
+        .replace("{file}", file)
+        .replace("{name}", name)
+        .replace("{ext}", ext);
 	    
 	if cmd.is_empty() {
-		println!("This file type is currently unsupported :(");
-		exit(1);
+        return Err(String::from("This file type is currently unsupported"));
 	}
 	let pargs: String = args.iter().skip(2).cloned().collect::<Vec<String>>().join(" ");
+
     let status = Command::new("sh").arg("-c").arg(format!("{} {}", cmd, pargs)).status();
     match status {
-        Ok(_) => {},
+        Ok(_) => {
+            return Ok(String::new());
+        },
         Err(err) => {
             println!("Error executing command: {}", err);
+            exit(1);
         }
     }
-    exit(0);
 }
