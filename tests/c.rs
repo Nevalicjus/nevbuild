39e3fbe268cf6c6841d212149a0f2dede5c70b8f
Maciej Bromirski <maciej.bromirski@gmail.com>
2025-05-14 09:44:03 +0200

    removed unused tests

--- tests/c.rs
+++
@@ -1,8 +0,0 @@
-use std::fs;
-
-#[test]
-fn test_c() {
-	let _ = fs::File::create("test.c").expect("");
-	let s: &[u8] = "#include <stdio.h>\n\nint main() {\n    printf(\"Hello, World!\\n\");\n}\n".as_bytes();
-	let _ = fs::write("test.c", &s);
-}
